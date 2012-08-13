ada1dd68ea4d78386d6689b532b0ac5321e4d0bb
tynewstar <tynewstar@126.com>
2012-08-13 16:01:34 +0800

    create the repository to Three-Killed-Game.

---
+++ Projects/ThreeKilled/ThreeKilled/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ThreeKilled
+//
+//  Created by Liu Mingxing on 8/13/12.
+//  Copyright (c) 2012 Symbio Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
